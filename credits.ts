476568a1409b1b441d7e40a7464d4bf16b35eb35
Magne Skjæran <github@meneth.com>
2026-01-25 22:07:53 +0100

    Credits update

--- credits.ts
+++ credits.ts
@@ -2,9 +2,10 @@ export const CREDITS =
  "Game created by Magne 'Meneth' Skjæran<br><br>"
  + "Inspired by Prismatic Adventure and Increlution<br><br>"
  + "<h2>Special Thanks</h2>"
- + "Zwemvest, for feedback, playtesting, and code review<br>"
+ + "Groogy, for feedback<br>"
  + "Jenny Johansson, for feedback and playtesting<br>"
- + "Groogy, for detailed feedback<br>"
- + "Mister_Kipper, for detailed feedback<br>"
+ + "Ksenia Zhitomirskaya, for feedback, playtesting, and giving me the push to start making this game<br>"
+ + "Mister_Kipper, for feedback<br>"
+ + "Zwemvest, for feedback, playtesting, and code review<br>"
  + "Everyone else who has given feedback<br>"
 ;
